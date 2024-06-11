4202c6f4272078f50469e565db8822fe05e610ac
Yatika Jena <j.yatika@iitg.ac.in>
2024-06-11 16:40:22 +0530

    ProfilePage updated

--- lib/profile/ProfilePage.dart
+++ lib/profile/ProfilePage.dart
@@ -30,30 +30,37 @@ class _profile_pageState extends State<profile_page> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                children: [
-                  CircleAvatar(
-                    radius: 30,
-                    //backgroundImage: AssetImage('assets/profile_picture.jpg'),
-                  ),
-                  SizedBox(width: 16),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Abhishek Sinha',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
+              Container(
+                decoration: BoxDecoration(
+                    color: MyColors.navBarBackgroundColor,
+                    borderRadius: BorderRadius.circular(8)),
+                padding: EdgeInsets.all(16),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                      //backgroundImage: AssetImage('assets/profile_picture.jpg'),
+                    ),
+                    SizedBox(width: 16),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Abhishek Sinha',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      Text('ID 2209753728'),
-                    ],
-                  ),
-                ],
+                        Text('ID 2209753728'),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-              SizedBox(height: 16),
+              SizedBox(height: 26),
               ListTile(
+                tileColor: MyColors.navBarBackgroundColor,
                 leading: Icon(Icons.contacts),
                 title: Text('Important contacts'),
                 trailing: InkWell(
@@ -73,7 +80,9 @@ class _profile_pageState extends State<profile_page> {
                   ),
                 ),
               ),
+              // SizedBox(height: 16),
               ListTile(
+                tileColor: MyColors.navBarBackgroundColor,
                 leading: Icon(Icons.feedback),
                 title: Text('Feedback'),
                 trailing: InkWell(
@@ -93,7 +102,9 @@ class _profile_pageState extends State<profile_page> {
                   ),
                 ),
               ),
+              //SizedBox(height: 16),
               ListTile(
+                tileColor: MyColors.navBarBackgroundColor,
                 leading: Icon(Icons.help),
                 title: Text('FAQs'),
                 trailing: InkWell(
