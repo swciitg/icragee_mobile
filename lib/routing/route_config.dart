fd3c7a5b55b7b1ea84ead07958a6964d5e6c21df
Aradhya <aradhya8058@gmail.com>
2024-06-19 21:40:54 +0530

    Implemented the UI for SplashScreen GetStarted and navigation

--- lib/routing/route_config.dart
+++ lib/routing/route_config.dart
@@ -1,6 +1,9 @@
 import 'package:go_router/go_router.dart';
 import 'package:icragee_mobile/routing/app_routes.dart';
+import 'package:icragee_mobile/screens/home_screen.dart';
 import 'package:icragee_mobile/screens/splash.dart';
+
+import '../screens/get_started/get_started.dart';
 
 final routeConfig = GoRouter(
   initialLocation: '/',
@@ -9,6 +12,15 @@ final routeConfig = GoRouter(
       path: '/',
       name: AppRoutes.splashScreen.name,
       builder: (context, state) => const SplashScreen(),
-    )
+    ),
+    GoRoute(
+      path: '/get-started',
+      builder: (context, state) => const GetStarted(),
+    ),
+    GoRoute(
+      path: '/homeScreen',
+      builder: (context, state) => const HomeScreen(),
+    ),
   ],
 );
+
---
+++ lib/screens/get_started/get_started.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:icragee_mobile/screens/splash.dart';
+class GetStarted extends StatelessWidget {
+  const GetStarted({super.key});
+
+  //BuildContext get context => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+      home: buildScaffold(context),
+
+    );
+  }
+
+  Scaffold buildScaffold(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children:<Widget> [
+          Image.asset(
+            'assets/images/get_started_image.png', // Replace with your image path
+            fit: BoxFit.cover, // Cover the entire screen with the image
+          ),
+           Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:<Widget> [
+              const SizedBox(height: 560),
+             const Text(
+                "Lorem ipsum dolor",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w500,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const Text(
+                '''consectetur adipiscing elit, sed do  
+             eiusmod tempor incididunt ut labore et dolore
+                magna aliqua''',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10,
+                  fontWeight: FontWeight.w500,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  context.go('/homeScreen');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFFF8C40), // Background color
+                  foregroundColor: Colors.white, // Text color
+                  minimumSize: const Size(310, 51), // Width and height
+                  padding: const EdgeInsets.symmetric(vertical: 13), // Padding
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10), // Rounded corners
+                  ),
+                ),
+                child: const Text(
+                  "Get Started",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              )
+              ,
+
+            ],
+          )
+
+        ],
+      )
+
+    );
+  }
+}
