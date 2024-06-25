6ae5f3595ce996e54bb737dcebeda7a4424e7cf7
Amit Singh <134639939+amitsing8576@users.noreply.github.com>
2024-06-25 13:29:53 +0530

    removed the commented portion

--- lib/services/data_service.dart
+++ lib/services/data_service.dart
@@ -3,15 +3,7 @@ import 'package:icragee_mobile/models/faq.dart';
 
 class DataService {
   Future<List<Faqs>> fetchFaqs() async {
-    /*String jsonString = '''
-    [
-      {"Question": "What is Dart?", "Answer": "Dart is a programming language."},
-      {"Question": "What is Flutter?", "Answer": "Flutter is a framework for building natively compiled applications."}
-    ]
-    ''';
-
-    List<dynamic> faqData = jsonDecode(jsonString);
-    List<Faqs> faqsList = faqData.map((faq) => Faqs.fromJson(faq)).toList();*/
+   
 
     var collectionSnapshot =
         await FirebaseFirestore.instance.collection('FAQs').get();
