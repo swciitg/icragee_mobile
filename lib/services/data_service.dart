28f96a8bc41d369db7ca96b980c7448737ccf83c
Ramu Kumar <rk9120541@gmail.com>
2024-06-25 19:35:37 +0530

    feedbackpage

--- lib/services/data_service.dart
+++ lib/services/data_service.dart
@@ -1,1 +1,19 @@
-class DataService {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DataService {
+  static Future<void> submitFeedback({
+    required var name,
+    required var email,
+    required var feedback,
+  }) async {
+    try {
+      await FirebaseFirestore.instance.collection("feedback").add({
+        "name": name,
+        "email": email,
+        "feedback": feedback,
+      });
+    } catch (error) {
+      throw ('Failed to submit feedback: $error');
+    }
+  }
+}
