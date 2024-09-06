8290d9edd59844ec0fe0d7f8fde2a9231c03b3ff
Yatika Jena <j.yatika@iitg.ac.in>
2024-09-06 11:56:17 +0530

    "check description" reloading bug fixed

--- lib/widgets/event_card.dart
+++ lib/widgets/event_card.dart
@@ -1,171 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:icragee_mobile/models/schedule.dart';
-// import 'package:icragee_mobile/widgets/status_chip.dart';
-// import 'package:intl/intl.dart';
-//
-// class EventCard extends StatefulWidget {
-//   final Schedule event;
-//   final bool isExpanded;
-//   final VoidCallback onToggleDescription;
-//
-//   const EventCard({
-//     Key? key,
-//     required this.event,
-//     required this.isExpanded,
-//     required this.onToggleDescription,
-//   }) : super(key: key);
-//
-//   @override
-//   State<EventCard> createState() => _EventCardState();
-// }
-//
-// class _EventCardState extends State<EventCard> {
-//   String get eventStatus {
-//     final now = DateTime.now();
-//     if (now.isBefore(widget.event.startTime)) {
-//       return 'Upcoming';
-//     } else if (now.isAfter(widget.event.startTime) &&
-//         now.isBefore(widget.event.endTime)) {
-//       return 'Ongoing';
-//     } else {
-//       return 'Completed';
-//     }
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       decoration: BoxDecoration(
-//           color: Colors.white, borderRadius: BorderRadius.circular(8)),
-//       child: Padding(
-//         padding:
-//             const EdgeInsets.only(left: 16, top: 12, right: 16, bottom: 12),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             // Title and Status Row
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//               children: [
-//                 Expanded(
-//                   child: Text(
-//                     widget.event.title,
-//                     style: const TextStyle(
-//                         fontWeight: FontWeight.bold, fontSize: 16),
-//                     overflow: TextOverflow.visible,
-//                   ),
-//                 ),
-//                 StatusChip(status: eventStatus),
-//               ],
-//             ),
-//
-//             // Time and Location based on expansion
-//             if (widget.isExpanded)
-//               Row(
-//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                 children: [
-//                   Row(
-//                     children: [
-//                       const Icon(Icons.access_time, size: 14),
-//                       const SizedBox(width: 4),
-//                       Text(
-//                         '${DateFormat('kk:mm').format(widget.event.startTime.toLocal())}'
-//                         ' - ${DateFormat('kk:mm').format(widget.event.endTime.toLocal())}',
-//                         style: const TextStyle(
-//                             fontWeight: FontWeight.w500, fontSize: 14),
-//                       ),
-//                     ],
-//                   ),
-//                   SizedBox(
-//                     height: 8,
-//                   ),
-//                   Row(
-//                     children: [
-//                       const Icon(Icons.location_on, size: 14),
-//                       const SizedBox(width: 4),
-//                       Text(
-//                         widget.event.location,
-//                         style: TextStyle(fontSize: 14),
-//                       ),
-//                     ],
-//                   ),
-//                 ],
-//               )
-//             else
-//               Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Row(
-//                     children: [
-//                       const Icon(Icons.access_time, size: 14),
-//                       const SizedBox(width: 4),
-//                       Text(
-//                         '${DateFormat('kk:mm').format(widget.event.startTime.toLocal())}'
-//                         ' - ${DateFormat('kk:mm').format(widget.event.endTime.toLocal())}',
-//                         style: const TextStyle(
-//                             fontWeight: FontWeight.w500, fontSize: 14),
-//                       ),
-//                     ],
-//                   ),
-//                   SizedBox(
-//                     height: 8,
-//                   ),
-//                   Row(
-//                     children: [
-//                       const Icon(Icons.location_on, size: 14),
-//                       const SizedBox(width: 4),
-//                       Text(
-//                         widget.event.location,
-//                         style: TextStyle(fontSize: 14),
-//                       ),
-//                     ],
-//                   ),
-//                 ],
-//               ),
-//
-//             // Check Description button
-//             Padding(
-//               padding: EdgeInsets.only(bottom: 12),
-//               child: Center(
-//                 child: GestureDetector(
-//                   onTap: widget.onToggleDescription,
-//                   child: Row(
-//                     mainAxisAlignment: MainAxisAlignment.center,
-//                     children: [
-//                       const Text(
-//                         'Check Description',
-//                         style: TextStyle(
-//                             fontSize: 13,
-//                             color: Colors.teal,
-//                             decoration: TextDecoration.underline),
-//                       ),
-//                       SizedBox(
-//                         width: 2,
-//                       ),
-//                       Image.asset(
-//                         "assets/icons/check_description.png",
-//                         height: 18,
-//                         width: 18,
-//                         color: Colors.teal,
-//                       )
-//                     ],
-//                   ),
-//                 ),
-//               ),
-//             ),
-//
-//             // Description Text if expanded
-//             if (widget.isExpanded) ...[
-//               const SizedBox(height: 8),
-//               Text(widget.event.description),
-//             ],
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'dart:async';
 
 import 'package:flutter/material.dart';
@@ -175,14 +7,10 @@ import 'package:intl/intl.dart';
 
 class EventCard extends StatefulWidget {
   final Schedule event;
-  final bool isExpanded;
-  final VoidCallback onToggleDescription;
 
   const EventCard({
     Key? key,
     required this.event,
-    required this.isExpanded,
-    required this.onToggleDescription,
   }) : super(key: key);
 
   @override
@@ -191,6 +19,8 @@ class EventCard extends StatefulWidget {
 
 class _EventCardState extends State<EventCard> {
   late Timer _timer;
+  bool _isExpanded = false;
+
   late String _currentStatus;
 
   @override
@@ -221,6 +51,12 @@ class _EventCardState extends State<EventCard> {
     } else {
       return 'Finished';
     }
+  }
+
+  void _toggleDescription() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+    });
   }
 
   @override
@@ -285,7 +121,7 @@ class _EventCardState extends State<EventCard> {
               padding: EdgeInsets.only(top: 12, bottom: 12),
               child: Center(
                 child: GestureDetector(
-                  onTap: widget.onToggleDescription,
+                  onTap: _toggleDescription,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -310,7 +146,7 @@ class _EventCardState extends State<EventCard> {
             ),
 
             // Description Text if expanded
-            if (widget.isExpanded) ...[
+            if (_isExpanded) ...[
               const SizedBox(height: 8),
               Text(widget.event.description),
             ],
